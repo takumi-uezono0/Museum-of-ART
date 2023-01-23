28c7dc26c39e5bf9d677b712929d024f9548dbba
takumi-uezono0 <uezono1998@gmail.com>
2023-01-23 21:23:25 +0900

    create:Contact-page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Production } from "./pages/Production";
 import { Cart } from "./pages/Cart";
+import { Contact } from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Production />} />
+          <Route path="/production" element={<Production />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+export const Contact = () => {
+  return (
+    <>
+      <Header />
+      <h1>Contact!</h1>
+      <Footer />
+    </>
+  );
+};
