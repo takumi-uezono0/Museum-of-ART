d0e71a2fc30a8571f496b853c33282e478d90c32
takumi-uezono0 <uezono1998@gmail.com>
2023-01-24 05:13:49 +0900

    fix:Contact-css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useCallback } from "react";
 import Button from "@material-ui/core/Button";
-import TextInput from ".";
+import TextInput from "./Contact's";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
+import "./Contact's/Contact.css";
 
 export const Contact = (props) => {
   const [description, setDescription] = useState("");
@@ -88,38 +89,50 @@ export const Contact = (props) => {
   };
 
   return (
-    <div open={props.open} onClose={props.handleClose}>
+    <div open={props.open} onClose={props.handleClose} className="ContactPage">
       <Header />
-      <TextInput
-        label={"名前(必須)"}
-        multiline={false}
-        rows={1}
-        value={name}
-        type={"text"}
-        onChange={inputName}
-      />
-      <TextInput
-        label={"メールアドレス(必須)"}
-        multiline={false}
-        rows={1}
-        value={email}
-        type={"email"}
-        onChange={inputEmail}
-      />
-      <TextInput
-        label={"お問い合わせ内容(必須)"}
-        multiline={true}
-        rows={5}
-        value={description}
-        type={"text"}
-        onChange={inputDescription}
-      />
-      <Button onClick={props.handleClose} color="primary">
+      <p className="sectionTitle">
+        <span>C</span>ontact
+      </p>
+      <div className="ContactForm">
+        <p>当サイトについてご意見・ご要望をお聞かせ下さい。</p>
+        <TextInput
+          label={"名前(必須)"}
+          multiline={false}
+          rows={1}
+          value={name}
+          type={"text"}
+          onChange={inputName}
+        />
+        <TextInput
+          label={"メールアドレス(必須)"}
+          multiline={false}
+          rows={1}
+          value={email}
+          type={"email"}
+          onChange={inputEmail}
+        />
+        <TextInput
+          label={"お問い合わせ内容(必須)"}
+          multiline={true}
+          rows={10}
+          value={description}
+          type={"text"}
+          onChange={inputDescription}
+        />{" "}
+      </div>
+      <Button
+        onClick={props.handleClose}
+        variant="outlined"
+        color="error"
+        id="CancelButton"
+      >
         キャンセル
       </Button>
-      <Button onClick={submitForm} color="primary">
-        送信する
+      <Button onClick={submitForm} variant="contained" id="SendButton">
+        send
       </Button>
+
       <Footer />
     </div>
   );
