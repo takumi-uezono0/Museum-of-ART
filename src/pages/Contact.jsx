20194e36faa57bdc24d6869456de6647863d7cab
takumi-uezono0 <uezono1998@gmail.com>
2023-01-23 23:11:31 +0900

    fix:regex

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -32,8 +32,7 @@ export const Contact = (props) => {
   );
 
   const validateEmailFormat = (email) => {
-    const regex =
-      /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
     return regex.test(email);
   };
 
