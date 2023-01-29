3cf5e634361fab936298fb3e3bb8e9b41b3305d9
手島航平 <teshimakouhei@teshimakouheinoMacBook-Air.local>
2023-01-29 21:23:14 +0900

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,28 @@ export const Home = () => {
   return (
     <>
       <Header />
-      <h1>Home</h1>
+      <div className="Home1">
+        <p>時間の表示</p>
+        <li>
+          <ul>Shopping cart</ul>
+          <ul>Production</ul>
+          <ul>Contact</ul>
+        </li>
+        <li>
+          <ul>Contemporay Museum of Art</ul>
+        </li>
+        <p>画像の配置</p>
+        <p>現代アートをもっと身近に</p>
+        <p>
+            日本人はアートと縁がない、裕福な家庭の娯楽という<br />
+            認識を払拭したい。<br />
+            そんな気持ちで当サイトを作成しました。<br />
+            今回は【若者】をターゲットに<br />
+            お洒落で親しみやすい作品をピックアップ！<br />
+            ぜひバーチャルポップアップ美術館をお楽しみください。<br />
+        </p>
+        
+    </div>
       <Footer />
     </>
   );
