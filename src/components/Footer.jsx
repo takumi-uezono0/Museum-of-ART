4adc878635c3aef7717f0e653ff32efee492a821
小西里奈 <konishirina@konishirinanoMacBook-Air.local>
2023-01-30 20:04:08 +0900

    creat:footer-component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-
+import Image from "./images/logo-1.png";
+import "./Footer.css";
 export const Footer = () => {
+
   return (
-    <>
-      <a className="logo" href="/products">
-        Cute Puppies Express! on Footer!
-      </a>
-    </>
+    <div className="footer-flame">
+      <img src={Image} alt="logo"/>
+      <nav class="M"> 
+        <ul>
+            <li><a id="cart" href="/cart">shopping cart</a></li>
+              <li>/</li>
+            <li><a id="product" href="/product">production</a></li>
+              <li>/</li>
+            <li><a id="contact" href="/contact">cantact</a></li>
+        </ul>
+        </nav>
+        
+      <div class="C">
+               <p><small>&copy;KEN school</small></p>
+      </div>
+    </div>
   );
 };
