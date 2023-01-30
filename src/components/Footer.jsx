d970141c867d626e51130051dbfbb371fd608ef3
takumi-uezono0 <uezono1998@gmail.com>
2023-01-30 21:49:51 +0900

    fix:header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,35 @@ import React from "react";
 import Image from "./images/logo-1.png";
 import "./Footer.css";
 export const Footer = () => {
-
   return (
     <div className="footer-flame">
-      <img src={Image} alt="logo"/>
-      <nav class="M"> 
+      <nav class="M">
+        <img src={Image} alt="logo" />
         <ul>
-            <li><a id="cart" href="/cart">shopping cart</a></li>
-              <li>/</li>
-            <li><a id="product" href="/product">production</a></li>
-              <li>/</li>
-            <li><a id="contact" href="/contact">cantact</a></li>
+          <li>
+            <a id="cart" href="/cart">
+              shopping cart
+            </a>
+          </li>
+          <li>/</li>
+          <li>
+            <a id="product" href="/product">
+              production
+            </a>
+          </li>
+          <li>/</li>
+          <li>
+            <a id="contact" href="/contact">
+              cantact
+            </a>
+          </li>
         </ul>
-        </nav>
-        
+      </nav>
+
       <div class="C">
-               <p><small>&copy;KEN school</small></p>
+        <p>
+          <small>&copy;2023 KOUHEI MINAMI MASAMI RINA TAKUMI</small>
+        </p>
       </div>
     </div>
   );
