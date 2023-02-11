8688d94b3c059be2a649afbca5b2866ad1709894
takumi-uezono0 <uezono1998@gmail.com>
2023-02-11 19:04:45 +0900

    fix:header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,27 @@ export const Header = () => {
   const onClickButton = () => [setOpenMenu(!openMenu)];
   return (
     <header>
+      <button onClick={onClickButton}>Menu</button>
       {openMenu && (
         <>
+          <br />
           <a className="logo" href="/production">
             作品一覧へ
           </a>
+          <br />
           <a className="logo" href="/">
             MAPへ
           </a>
+          <br />
           <a className="logo" href="/cart">
             ShoppngCartへ
           </a>
+          <br />
           <a className="logo" href="/contact">
             お問い合わせへ
           </a>
         </>
       )}
-
-      <button onClick={onClickButton}>Menu</button>
     </header>
   );
 };
